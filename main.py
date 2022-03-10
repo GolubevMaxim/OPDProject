819d55c0525d743e2b0ac381f408d4702c54b4c0
GolubevMaxim <vfrc008@icloud.com>
2022-03-10 18:15:23 +0300

    added simple array drawing class

--- main.py
+++ main.py
@@ -1,18 +1,75 @@
+import random
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
 
 
-class MainWindow(QMainWindow):
+class App(QMainWindow):
 
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
+        self.arrayPainter = None
+        self.title = 'OPD Project'
 
-        self.setWindowTitle("OPD Project")
+        # screen settings
+        self.left = 100
+        self.top = 100
+        self.width = 440
+        self.height = 280
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # Set window background color
+        self.setAutoFillBackground(True)
+        p = self.palette()
+        p.setColor(self.backgroundRole(), QColor(255, 255, 255))
+        self.setPalette(p)
+
+        # Add paint widget and paint
+        arr = [[random.randint(0, 1) for _ in range(10)] for _ in range(10)]
+        self.arrayPainter = PaintWidget(array=arr, parent=self)
+
+        self.arrayPainter.move(0, 0)
+        self.arrayPainter.resize(self.width, self.height)
+
+        self.show()
 
 
-app = QApplication(sys.argv)
+class PaintColors:
+    carColor = QColor(80, 80, 80)
+    emptyColor = QColor(255, 255, 255)
 
-window = MainWindow()
-window.show()
 
-app.exec()
+class PaintWidget(QWidget):
+    def __init__(self, array, parent=None):
+        super().__init__(parent)
+        self.array = array
+
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        qp.setPen(QColor(0, 0, 0))
+
+        size = self.size()
+
+        # Draw array
+        for i, row in enumerate(self.array):
+            for j, elem in enumerate(row):
+                qp.setBrush(PaintColors.carColor if elem else PaintColors.emptyColor)
+
+                qp.drawRect(
+                    int((i / len(self.array)) * size.width()),
+                    int((j / len(self.array)) * size.height()),
+                    int(((i + 1) / len(self.array)) * size.width()),
+                    int(((j + 1) / len(self.array)) * size.height())
+                )
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
